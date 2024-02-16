ba9af86acd4aa0ac86b88693e64715104d637ab1
angryman <koreom@gmail.com>
2024-02-16 12:37:14 +0900

    서버생성 및 npm 설치

---
+++ server/app.js
@@ -0,0 +1,48 @@
+const express=require('express');
+const path=require('path');
+const morgan=require('morgan');
+const cookieParser=require('cookie-parser');
+const session=require('express-session');
+const dotenv=require('dotenv');
+const passport=require('passport');
+
+dotenv.config();
+
+const app=express();
+
+app.set('port',process.env.PORT||3000);
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname,'public')))
+app.use(express.json())
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+app.use(session({
+    resave:false,
+    saveUninitialized:false,
+    secret:process.env.COOKIE_SECRET,
+    cookie:{
+        httpOnly:true,
+        secure:false
+    }
+}));
+
+app.use((req,res,next)=>{
+    const error=new Error(`${req,method} ${req.method} 라우터가 없습니다.`);
+    error.status=404;
+    next(error);
+});
+
+app.use((err,req,res,next)=>{
+    res.locals.message=err.message;
+    res.locals.error=process.env.NODE_ENV !=='production'? err:{};
+    res.status(err.status||500);
+    res.render('error');
+    next();
+});
+
+const port=app.get('port');
+app.listen(port,()=>{
+    console.log(port,"포트 서버 대기");
+})
