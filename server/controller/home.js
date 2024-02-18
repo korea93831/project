ab313d0ce0e611388c34d59256f4147e0344294b
angryman <koreom@gmail.com>
2024-02-19 00:20:27 +0900

    db create test

--- server/controller/home.js
+++ server/controller/home.js
@@ -1,12 +1,24 @@
-const { Product } = require("../models");
+const model=require('../models')
 
 exports.Main=(req,res,next)=>{
-    const getTop20User=async(req,res)=>{
-        let users=await Product.findAndCountAll({
-            offset: 10,
-            limit:2
+   
+        let info={
+            email:'1111@gmail.com',
+            nickname:'admin',
+            password:'0000',
+        };
+        model.User.create(info)
+        .then((result)=>{
+            res.send({email:result.email})
         })
-    }
+    
+    // const getTop20User=async(req,res)=>{
+    //     let users=await Product.findAndCountAll({
+    //         offset: 10,
+    //         limit:2
+    //     })
+    // }
+    // res.send(getTop20User);
     console.log('req main');
     res.send('req main');
 }
